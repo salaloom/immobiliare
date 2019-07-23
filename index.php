9bbe9467f5407aab5845fec679089b2ca3c86b88
Mister V <salimhalla@gmail.com>
2019-07-23 15:03:14 +0200

    -

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="utf-8">
+        <title>Mon thème WordPress</title>
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    </head>
+    <body>
+        <h1>Bienvenue sur le site <?php bloginfo('name'); ?></h1>
+        <p><?php bloginfo('description'); ?></p>
+    </body>
+</html>
