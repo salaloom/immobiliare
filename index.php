1ce696959cdeb6f8991511597ed55cf30b53444e
Mister V <salimhalla@gmail.com>
2019-07-23 17:18:31 +0200

    first style mods

--- index.php
+++ index.php
@@ -6,7 +6,58 @@
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     </head>
     <body>
-        <h1>Bienvenue sur le site <?php bloginfo('name'); ?></h1>
-        <p><?php bloginfo('description'); ?></p>
+
+
+
+    <nav  class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#"><?php bloginfo('name'); ?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+             </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+   
+     <div class="container">
+
+                <h1 class="text-center my-4">Bienvenue sur le site <?php bloginfo('name'); ?></h1>
+                <p><?php bloginfo('description'); ?></p>
+
+    </div>
+
+
     </body>
 </html>
